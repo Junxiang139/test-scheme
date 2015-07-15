108993b5779a7b78cd227ca389017ea76d1a498d
dolphinauditore <841450297@qq.com>
2015-07-15 18:52:04 +0800

    this time true lambda

--- sum-for-all.rkt
+++ sum-for-all.rkt
@@ -6,7 +6,11 @@
 (define (inc a) (+ a 1))
 (define (cube a) (* a a a))
 (define (pi-term a) (/ 1.0
-                       (* a (+ a 2))))
-(define (pi-next a) (+ a 4))
-(define (getpi a b)
-  (* 8 (sum 1 1000 pi-term pi-next)))
+                       (* a (+ a 2.0))))
+(define (pi-next a) (+ a 4.0))
+(define (getpi b)
+  (* 8.0 (sum 1 b pi-term pi-next)))
+(define (getpi2 b)
+  (* 8.0 (sum 1 b
+              (lambda (a) (/ 1.0 (* a (+ a 2.0))))
+              (lambda (a) (+ a 4.0)))))
