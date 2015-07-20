41bc51183fba90db5ff79eb8ac4515c3d794150b
dolphinauditore <841450297@qq.com>
2015-07-20 17:14:50 +0800

    try push

--- changecoins2.rkt
+++ changecoins2.rkt
@@ -10,4 +10,4 @@
                      coin-values)))))
 (define (no-more? coin-values) (null? coin-values))
 (define (first-denomination coin-values) (car coin-values))
-(define (except-first-denomination coin-values) (cdr coin-values))
+(define (except-first-denomination coin-values) (cdr coin-values)) 
